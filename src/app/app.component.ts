efa924c66cd40ef1ecc51daad5da905e52e5dae0
UlisesC12 <39888086+UlisesC12@users.noreply.github.com>
2023-01-22 03:30:33 -0600

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  register = {
+    name: '',
+    email: '',
+    password: '',
+  }
+  onRegister() {
+    console.log(this.register)
+  }
 }
